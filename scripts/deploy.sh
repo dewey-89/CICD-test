55177cbb1b99bc038a53233fdc55da79b424db3f
dewey89 <140939516+dewey-89@users.noreply.github.com>
2023-10-24 23:05:35 +0900

    CICD test!!

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,10 @@
 cd /home/ubuntu/app
 
 # 환경변수 설정
-DOCKER_APP_NAME="spring"
+DOCKER_APP_NAME="docker"
 
 # 현재 실행 중인 "blue" 컨테이너 확인
-EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep "spring-blue" | grep Up)
+EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep "docker-blue" | grep Up)
 
 # 배포 시작일자 기록
 echo "배포 시작일자 : $(date '+%Y-%m-%d %H:%M:%S')" >> /home/ubuntu/deploy.log
