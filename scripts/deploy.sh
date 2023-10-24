b669061acbf9f4352ac557e55632425b64c9c069
dewey89 <140939516+dewey-89@users.noreply.github.com>
2023-10-24 23:33:42 +0900

    CICD test!!

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ cd /home/ubuntu/app
 DOCKER_APP_NAME="docker"
 
 # 현재 실행 중인 "blue" 컨테이너 확인
-EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep "docker-blue")
+EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
 
 # 배포 시작일자 기록
 echo "배포 시작일자 : $(date '+%Y-%m-%d %H:%M:%S')" >> /home/ubuntu/deploy.log
