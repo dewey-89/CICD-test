ee5c785d1f17850f59af9fcf9f5ebbe47febbb0e
dewey89 <140939516+dewey-89@users.noreply.github.com>
2023-10-24 22:01:40 +0900

    CICD green test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ DOCKER_APP_NAME=spring
 
 # 실행중인 blue가 있는지 확인
 # 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지 여부를 EXIST_BLUE 변수에 저장
-EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
 
 # 배포 시작한 날짜와 시간을 기록
 echo "배포 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
