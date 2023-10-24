a02a2608e9cb8a95c322ef38709892731c8ee35f
dewey89 <140939516+dewey-89@users.noreply.github.com>
2023-10-24 22:19:04 +0900

    CICD green test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ DOCKER_APP_NAME=spring
 
 # 실행중인 blue가 있는지
 EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
-
+#
 # green이 실행중이면 blue up
 if [ -z "$EXIST_BLUE" ]; then
 	echo "blue up"
